2fcc6f76d96d54c1216760b6c1c1b6485b8bf51e
VISHNU <vishnubiradar576@gmail.com>
2024-02-17 18:21:20 +0530

    My Code

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
